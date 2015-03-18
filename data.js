a2282e209881e659ce8a2e112a6a2c79962d5cfe
Charlie Park <charlie@charliepark.org>
2015-03-17 19:15:03 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+[
+  {
+    "publishedOn":"2015-03-24",
+    "guest":"Una Kravets",
+    "guestPhoto":"https://cloud.githubusercontent.com/assets/22547/6701291/46e1eb56-ccd8-11e4-8b95-5473ccb7a44b.jpg",
+    "guestTwitter":"una",
+    "summary":"TK"
+  }
+]
