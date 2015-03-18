5f3c737387a098a958bac550d7aa010421e797da
Charlie Park <charlie@charliepark.org>
2015-03-17 19:17:10 -0700

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-[
-  {
-    "publishedOn":"2015-03-24",
-    "guest":"Una Kravets",
-    "guestPhoto":"https://cloud.githubusercontent.com/assets/22547/6701291/46e1eb56-ccd8-11e4-8b95-5473ccb7a44b.jpg",
-    "guestTwitter":"una",
-    "summary":"TK"
-  }
-]
